2941fdf57dabda30442250b602e919e780b42828
fky <fky@DSASH-FKY>
2015-12-16 11:09:09 +0800

    remove set/get method

--- com.zml.eclipse.powermode/src/com/zml/eclipse/powermode/explosion/Explosion.java
+++ com.zml.eclipse.powermode/src/com/zml/eclipse/powermode/explosion/Explosion.java
@@ -33,54 +33,6 @@ public class Explosion {
 		this.particles = particles;
 	}
 
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public float getGravity() {
-		return gravity;
-	}
-
-	public void setGravity(float gravity) {
-		this.gravity = gravity;
-	}
-
-	public float getWind() {
-		return wind;
-	}
-
-	public void setWind(float wind) {
-		this.wind = wind;
-	}
-
-	public int getSize() {
-		return size;
-	}
-
-	public void setSize(int size) {
-		this.size = size;
-	}
-
-	public int getState() {
-		return state;
-	}
-
-	public void setState(int state) {
-		this.state = state;
-	}
-
 	// helper methods -------------------------
 	public boolean isAlive() {
 		return this.state == STATE_ALIVE;
