952e2480c5c40013fe70ef70109e3f663ac50896
zml <zmlcoder@gmail.com>
2015-12-16 23:15:03 +0800

    Do optimization for explosion painter.

--- com.zml.eclipse.powermode/src/com/zml/eclipse/powermode/explosion/ExplosionPainter.java
+++ com.zml.eclipse.powermode/src/com/zml/eclipse/powermode/explosion/ExplosionPainter.java
@@ -5,6 +5,9 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IPaintPositionManager;
@@ -16,90 +19,156 @@ import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Display;
 
+/**
+ * The explosion painter.
+ * 
+ * @author zml
+ *
+ */
 public class ExplosionPainter implements IPainter, PaintListener {
 
-	/** Indicates whether this painter is active */
-	private boolean fIsActive = false;
-
 	/** The source viewer this painter is associated with */
-	private final ISourceViewer fSourceViewer;
+	private final ISourceViewer mSourceViewer;
 
 	/** The viewer's widget */
-	private final StyledText fTextWidget;
+	private final StyledText mTextWidget;
+
+	/** The thread for animation pump. */
+	private final ExecutorService mExecutorService;
+
+	/** The set to collect explosions. */
+	private final Set mExplosionSet = new LinkedHashSet();
+
+	/** The lock for the explosion set. */
+	private final ReadWriteLock mLock = new ReentrantReadWriteLock();
+
+	/** Indicates whether this painter is active */
+	private boolean mIsActive = false;
 
 	/** The paint position manager */
-	private IPaintPositionManager fPaintPositionManager;
+	private IPaintPositionManager mPaintPositionManager;
 
-	private Set explosionSet = new LinkedHashSet();
+	private boolean mIsRuning = false;
 
-	private boolean isRuning = false;
+	private int mPrevious;
 
-	private ExecutorService executor;
-
-	private int previous;
-
+	/**
+	 * Constructor.
+	 * 
+	 * @param sourceViewer
+	 *            The source viewer.
+	 */
 	public ExplosionPainter(ISourceViewer sourceViewer) {
-		fSourceViewer = sourceViewer;
-		fTextWidget = sourceViewer.getTextWidget();
-		executor = Executors.newSingleThreadExecutor();
+		mSourceViewer = sourceViewer;
+		mTextWidget = sourceViewer.getTextWidget();
+		mExecutorService = Executors.newSingleThreadExecutor();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void paintControl(PaintEvent e) {
-		Iterator iterator = explosionSet.iterator();
-		while (iterator.hasNext()) {
-			Explosion explosion = (Explosion) iterator.next();
-			if (explosion.isAlive()) {
-				explosion.update();
-				explosion.draw(e.gc);
-			} else {
-				iterator.remove();
-				explosion.dispose();
+		Iterator iterator = null;
+
+		Lock readLock = mLock.readLock();
+		readLock.lock();
+		try {
+			iterator = new LinkedHashSet(mExplosionSet).iterator();
+		} finally {
+			readLock.unlock();
+		}
+		if (iterator != null) {
+			while (iterator.hasNext()) {
+				Explosion explosion = (Explosion) iterator.next();
+				if (explosion.isAlive()) {
+					explosion.draw(e.gc);
+				}
 			}
 		}
+
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void dispose() {
-		executor.shutdown();
+		mExecutorService.shutdown();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void paint(int reason) {
 
-		if (!fIsActive) {
-			fIsActive = true;
-			fTextWidget.addPaintListener(this);
+		if (!mIsActive) {
+			mIsActive = true;
+			mTextWidget.addPaintListener(this);
 		}
 
-		IDocument document = fSourceViewer.getDocument();
+		IDocument document = mSourceViewer.getDocument();
 		if (document == null) {
 			deactivate(false);
 			return;
 		}
 		int length = document.getLength();
-		if (reason == IPainter.TEXT_CHANGE && previous != length) {
-			previous = length;
-			Point location = fTextWidget.getLocationAtOffset(fTextWidget.getCaretOffset());
-			explosionSet.add(new Explosion(10, location.x, location.y + 5));
+		if (reason == IPainter.TEXT_CHANGE && mPrevious != length) {
+			mPrevious = length;
+			Point location = mTextWidget.getLocationAtOffset(mTextWidget.getCaretOffset());
+			Lock writeLock = mLock.writeLock();
+			writeLock.lock();
+			try {
+				mExplosionSet.add(new Explosion(10, location.x, location.y + 5));
+			} finally {
+				writeLock.unlock();
+			}
 			fire();
 		}
-
 	}
 
+	/**
+	 * Fire one new animation.
+	 */
 	private void fire() {
 
-		if (isRuning) {
+		if (mIsRuning) {
 			return;
 		}
 
-		isRuning = true;
-		executor.execute(new Runnable() {
+		mIsRuning = true;
+		mExecutorService.execute(new Runnable() {
 			public void run() {
 
-				while (explosionSet.size() > 0) {
-					Display.getDefault().syncExec(new Runnable() {
+				while (mExplosionSet.size() > 0) {
+
+					// Update all valid explosions data.
+					Iterator iterator = null;
+					Lock readLock = mLock.readLock();
+					readLock.lock();
+					try {
+						iterator = new LinkedHashSet(mExplosionSet).iterator();
+					} finally {
+						readLock.unlock();
+					}
+					while (iterator.hasNext()) {
+						Explosion explosion = (Explosion) iterator.next();
+						if (explosion.isAlive()) {
+							explosion.update();
+						} else {
+							iterator.remove();
+							explosion.dispose();
+						}
+					}
+
+					// Trigger the redraw event.
+					Display.getDefault().asyncExec(new Runnable() {
 						public void run() {
-							fTextWidget.redraw();
+							if (mTextWidget != null && !mTextWidget.isDisposed()) {
+								mTextWidget.redraw();
+							}
 						}
 					});
+
+					// Sleep 0.1s to improve user experience.
 					try {
 						Thread.currentThread().sleep(100);
 					} catch (InterruptedException e) {
@@ -107,20 +176,26 @@ public class ExplosionPainter implements IPainter, PaintListener {
 					}
 				}
 
-				isRuning = false;
+				mIsRuning = false;
 			}
 		});
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void deactivate(boolean redraw) {
-		if (fIsActive) {
-			fIsActive = false;
-			fTextWidget.removePaintListener(this);
+		if (mIsActive) {
+			mIsActive = false;
+			mTextWidget.removePaintListener(this);
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setPositionManager(IPaintPositionManager manager) {
-		fPaintPositionManager = manager;
+		mPaintPositionManager = manager;
 	}
 
 }
